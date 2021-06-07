145574c02d2fba68f67a5c51226840c551084765
brkss <berkassebrahim@gmail.com>
2021-06-07 01:07:07 +0100

    ✨ Toggle home and about

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,19 @@
 
 
 const links = {
-  
+  about: document.getElementById('about-l'),
+  projects: document.getElementById('projects-l'),
+  contact: document.getElementById('contact-l')
 }
+const pages = {
+  home: document.getElementById('home'),
+  about: document.getElementById('about')
+}
+
+links.about.addEventListener('click', () => {
+  console.log('you clicked about')
+  pages.home.style.display = 'none';
+  pages.about.style.display = 'block';
+});
+
+console.log('links => ', links);
