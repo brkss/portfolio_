dc90c4788843fe5c617ba2977d0f173052ddb4b3
brkss <berkassebrahim@gmail.com>
2021-06-07 01:15:01 +0100

    ⚡️ back buttom

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 
 
 const links = {
+  home: document.getElementById('back'),
   about: document.getElementById('about-l'),
   projects: document.getElementById('projects-l'),
   contact: document.getElementById('contact-l')
@@ -10,8 +11,17 @@ const pages = {
   about: document.getElementById('about')
 }
 
+
+// back home 
+links.home.addEventListener('click', () => {
+  links.home.style.display = 'none'
+  pages.about.style.display = 'none'
+  pages.home.style.display = 'block'
+});
+
 links.about.addEventListener('click', () => {
   console.log('you clicked about')
+  links.home.style.display = 'block'
   pages.home.style.display = 'none';
   pages.about.style.display = 'block';
 });
